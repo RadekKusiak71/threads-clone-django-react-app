5ff47786036959ec0b0b7bac11b4bd42caff556a
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-26 11:01:52 +0200

    simple_jwt setup

--- backend/api_authentication/views.py
+++ backend/api_authentication/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
