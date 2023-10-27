76aa520b8bf7f39ac31b9b0a510ca9e5b525a3bf
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-27 16:09:39 +0200

    cors headers setup + db records added

--- backend/Threads/settings.py
+++ backend/Threads/settings.py
@@ -93,7 +93,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -102,6 +102,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'Threads.urls'
@@ -171,8 +172,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#MEDIA ROOT SETUP
-MEDIA_URL ='./media/'
+# MEDIA ROOT SETUP
+MEDIA_URL = './media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
