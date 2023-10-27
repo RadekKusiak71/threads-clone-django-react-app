df2dac6a6dbb316e31d9ec62508a1ba9e8a418a7
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-27 18:28:02 +0200

    added update , login and refresh logic

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { Children, createContext, useEffect, useState } from 'react'
-import jwt_decode from 'jwt-decode'
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext()
@@ -7,8 +7,9 @@ const AuthContext = createContext()
 export default AuthContext
 
 export const AuthProvider = ({ children }) => {
-    const [authTokens, setAuthToknes] = useState(null)
-    const [user, setUser] = useState(null)
+
+    let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')) : null)
     const [error, setError] = useState(null)
     const navigate = useNavigate()
 
@@ -24,28 +25,68 @@ export const AuthProvider = ({ children }) => {
             });
             if (response.ok) {
                 let data = await response.json();
-                console.log(data);
                 navigate('/login');
             } else {
                 let errorData = await response.json();
-                console.log(errorData);
                 setError(errorData);
             }
         } catch (err) {
             console.log(err);
         }
     };
-    const loginUser = async () => {
-        console.log('login')
+    const loginUser = async (formData) => {
+        try {
+            let response = await fetch('http://127.0.0.1:8000/authentication/token/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            let data = await response.json()
+            if (response.ok) {
+                setAuthTokens({ refresh: data.refresh, access: data.access });
+                setUser(jwtDecode(data.access));
+                localStorage.setItem('authTokens', JSON.stringify({ ...authTokens, access: data.access }));
+                navigate('/')
+            } else {
+                setError(data)
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    const logoutUser = () => {
-        console.log('logout')
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        navigate('/login')
     }
 
     const updateToken = async () => {
-        console.log('updating tokens')
+        try {
+            let response = await fetch('http://127.0.0.1:8000/authentication/token/refresh/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 'reftesh': authTokens.refresh })
+            })
+            let data = await response.json();
+
+            if (response.ok) {
+                setAuthTokens({ ...authTokens, access: data.access });
+                setUser(jwtDecode(data.access));
+                localStorage.setItem('authTokens', JSON.stringify({ ...authTokens, access: data.access }));
+            } else {
+                logoutUser();
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
+
 
     useEffect(() => {
         const refresher = 1000 * 60 * 14
@@ -61,7 +102,8 @@ export const AuthProvider = ({ children }) => {
     let authData = {
         user: user,
         registerUser: registerUser,
-        error:error
+        loginUser: loginUser,
+        error: error
     }
 
     return (
