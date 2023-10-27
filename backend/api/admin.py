85c73e15e8a960dc0392b45d91d9a1ed3425d8b8
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-27 14:21:01 +0200

    Adjusted Database + migrations and models register

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Thread, Post, Comment, CommentLike, PostLike
 
-# Register your models here.
+admin.site.register(Thread)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(CommentLike)
+admin.site.register(PostLike)
--- backend/api/models.py
+++ backend/api/models.py
@@ -10,7 +10,8 @@ def user_directory_path(instance, filename):
 
 class Thread(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to=user_directory_path)
+    profile_image = models.ImageField(
+        upload_to=user_directory_path, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
@@ -21,12 +22,17 @@ class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     body = models.TextField(max_length=280)
     created_date = models.DateTimeField(auto_now_add=True)
-    attachment = models.FileField(upload_to=user_directory_path, null=True, validators=[
+    attachment = models.FileField(upload_to=user_directory_path, null=True, blank=True, validators=[
                                   FileExtensionValidator(allowed_extensions=['mp4', 'mp3', 'png', 'jpge', 'jpg'])])
     likes = models.ManyToManyField(
         User, related_name="liked_posts", through='PostLike')
-    unlikes = models.ManyToManyField(
-        User, related_name="unliked_posts", through='PostUnlike')
+
+    def calculate_likes_count(self):
+        return self.likes.count()
+
+    def calculate_comments_count(self):
+        comments = Comment.objects.filter(post=self)
+        return comments.count()
 
 
 class Comment(models.Model):
@@ -35,8 +41,6 @@ class Comment(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(
         User, related_name="liked_comments", through='CommentLike')
-    unlikes = models.ManyToManyField(
-        User, related_name="unliked_comments", through='CommentUnlike')
 
 
 class PostLike(models.Model):
@@ -45,19 +49,7 @@ class PostLike(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
 
 
-class PostUnlike(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-
 class CommentLike(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-
-class CommentUnlike(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
