e0388c63e47cfbe41a44ea0c146acafc9b75498d
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-28 16:01:49 +0200

    changed input dynamic classes

--- frontend/src/UI/Input.js
+++ frontend/src/UI/Input.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import classes from './Input.module.css'
+import classNames from 'classnames';
 
 const Input = (props) => {
+  const inputClasses = classNames(classes['input'], props.className);
+
   return (
     <input
-        className={classes['input']}
-        type={props.type}
-        placeholder={props.placeholder}
-        name={props.name}
-        onChange={props.onChange}
+      className={inputClasses}
+      type={props.type}
+      placeholder={props.placeholder}
+      name={props.name}
+      onChange={props.onChange}
     />
   )
 }
