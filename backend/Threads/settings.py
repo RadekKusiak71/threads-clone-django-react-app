69540775e6ebce8b9e4eff8fe9aff6cb79c436a1
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-28 16:00:21 +0200

    fixed models + setting media root fix

--- backend/Threads/settings.py
+++ backend/Threads/settings.py
@@ -173,8 +173,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # MEDIA ROOT SETUP
-MEDIA_URL = './media/'
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/api/models.py
+++ backend/api/models.py
@@ -7,6 +7,9 @@ from django.core.validators import FileExtensionValidator
 def user_directory_path(instance, filename):
     return os.path.join('media', 'users', instance.user.username, filename)
 
+def user_directory_path_post(instance, filename):
+    return os.path.join('media', 'users', instance.thread.user.username, filename)
+
 
 class Thread(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,8 +25,8 @@ class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     body = models.TextField(max_length=280)
     created_date = models.DateTimeField(auto_now_add=True)
-    attachment = models.FileField(upload_to=user_directory_path, null=True, blank=True, validators=[
-                                  FileExtensionValidator(allowed_extensions=['mp4', 'mp3', 'png', 'jpge', 'jpg'])])
+    attachment = models.FileField(upload_to=user_directory_path_post, null=True, blank=True, validators=[
+                                  FileExtensionValidator(allowed_extensions=['mp4', 'mp3', 'png', 'jpeg', 'jpg'])])
     likes = models.ManyToManyField(
         User, related_name="liked_posts", through='PostLike')
 
