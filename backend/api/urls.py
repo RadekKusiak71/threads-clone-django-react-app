0ee534fb80d64e40b84ecead87cbb7f04db802cf
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-31 02:39:15 +0100

    added likes model viewset logic + post small fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path
-from .views import PostViewSet
+from django.urls import path, include
+from .views import PostViewSet, PostLikesViewSet
 
+post_router = DefaultRouter()
+post_router.register(r'posts', PostViewSet, basename='post')
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+likes_router = DefaultRouter()
+likes_router.register(r'likes', PostLikesViewSet, basename='like')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(post_router.urls)),
+    path('', include(likes_router.urls)),
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -13,7 +13,7 @@ class PostViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        queryset = Post.objects.all()
+        queryset = Post.objects.all().order_by('-created_date')
         serializer = CustomPostSerializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -31,10 +31,9 @@ class PostViewSet(viewsets.ViewSet):
 
     def create(self, request):
         user = request.user
-        data = request.data
+        data = request.data.copy()
         data['thread'] = data.get('thread_id')
         data['user'] = user.id
-        print(data)
 
         serializer = PostSerializer(data=data)
 
@@ -43,3 +42,43 @@ class PostViewSet(viewsets.ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostLikesViewSet(viewsets.ViewSet):
+    # permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['post'], url_path='like_post')
+    def like_post(self, request):
+        user = request.user
+        post_id = request.data.get('post_id')
+        
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'message': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        if user in post.likes.all():
+            return Response({'message': 'Post already liked.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        post_like = PostLike.objects.create(user=user, post=post)
+        post_like.save()
+
+        return Response({'message': 'Post liked successfully.'}, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['post'], url_path='unlike_post')
+    def unlike_post(self, request):
+        user = request.user
+        data = request.data
+        post_id = data.get('post_id')
+
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'message': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            post_like = PostLike.objects.get(user=user, post=post)
+            post_like.delete()
+            return Response({'message': 'Post unliked successfully.'}, status=status.HTTP_200_OK)
+        except PostLike.DoesNotExist:
+            return Response({'message': 'Post was not liked before.'}, status=status.HTTP_400_BAD_REQUEST)
