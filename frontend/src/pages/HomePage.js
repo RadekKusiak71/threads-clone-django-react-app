1fe76338da4cfe165082c9508ed5a6cbc087c79e
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-31 02:41:11 +0100

    HomePage post map dispalying

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import Card from '../UI/Card'
-import PostForm from '../components/PostForm'
-
+import React, { useContext, useEffect, useState } from 'react';
+import Card from '../UI/Card';
+import PostForm from '../components/PostForm';
+import Post from '../components/Post';
+import PostContext from '../context/PostContext';
 
 const HomePage = () => {
-
+    let { posts } = useContext(PostContext)
     return (
         <Card>
             <PostForm />
+            {posts.length > 0 ? (
+                posts.map((post) => (
+                    <Post
+                        key={post.id}
+                        postID={post.id}
+                        file={post.attachment}
+                        profileName={post.username}
+                        profileImg={post.profile_image}
+                        postBody={post.body}
+                        commentsCount={post.comments_count}
+                        likeCount={post.likes_count}
+                        thread={post.thread}
+                        postDate={post.created_date}
+                        likesArray={post.likes}
+                    />
+                ))
+            ) : (
+                <p>No posts available</p>
+            )}
         </Card>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
