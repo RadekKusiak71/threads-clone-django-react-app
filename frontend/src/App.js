35a01b4752a506334d46df5409c8573b24e9a22a
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-10-31 02:40:00 +0100

    added post provider + header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,25 @@ import RegisterPage from './pages/RegisterPage'
 import { AuthProvider } from './context/AuthContext'
 import HomePage from './pages/HomePage';
 import PrivateRoutes from './utils/PrivateRoutes';
+import { PostProvider } from './context/PostContext';
+import ThreadPage from './pages/ThreadPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route element={<PrivateRoutes />}>
-            <Route path='/' element={<HomePage />} />
-          </Route>
-        </Routes>
+        <PostProvider>
+          <Header />
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route element={<PrivateRoutes />}>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/thread/:username' element={<ThreadPage />} />
+            </Route>
+          </Routes>
+        </PostProvider>
       </AuthProvider>
     </BrowserRouter>
   );
