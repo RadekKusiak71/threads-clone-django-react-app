a98d0ae3d8585c4d984e686eb1949918806c777e
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-11-01 18:00:39 +0100

    moved checkingTimeAgo function to utils

---
+++ frontend/src/utils/TimeAgo.js
@@ -0,0 +1,28 @@
+function timeAgo(postDate) {
+    const postDateTime = new Date(postDate).getTime();
+    const now = new Date().getTime();
+    const timeDiff = now - postDateTime;
+
+    const seconds = Math.floor(timeDiff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+
+    if (years > 0) {
+        return `${years} year${years > 1 ? 's' : ''} ago`;
+    } else if (months > 0) {
+        return `${months} month${months > 1 ? 's' : ''} ago`;
+    } else if (days > 0) {
+        return `${days} day${days > 1 ? 's' : ''} ago`;
+    } else if (hours > 0) {
+        return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else if (minutes > 0) {
+        return `${minutes} min${minutes > 1 ? 's' : ''} ago`;
+    } else {
+        return `${seconds} sec${seconds !== 1 ? 's' : ''} ago`;
+    }
+}
+
+export default timeAgo;
